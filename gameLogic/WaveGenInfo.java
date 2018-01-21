7a7a5988c7160f3dbe67fecf0979700947c69fab
CathyWrHua <cathy.hua@gmail.com>
2018-01-20 16:13:44 -0800

    added wave generation information

---
+++ gameLogic/WaveGenInfo.java
@@ -0,0 +1,46 @@
+package gameLogic;
+
+import SquarePG.SquarePG;
+
+import java.util.Queue;
+
+public class WaveGenInfo {
+	public static final int GENERATION_DELAY = SquarePG.FPS*2;
+	private Queue<EnemyGenInfo> enemyGenInfoQueue;
+	private int delayCounter;
+
+	public WaveGenInfo(Queue<EnemyGenInfo> enemyQueue) {
+		enemyGenInfoQueue = enemyQueue;
+		delayCounter = GENERATION_DELAY;
+	}
+
+	public boolean shouldGenerateNextEnemy() {
+		return delayCounter <= 0;
+	}
+
+	public boolean waveIsComplete() {
+		return enemyGenInfoQueue.peek() == null;
+	}
+
+	public void decrementCounter() {
+		if (delayCounter > 0) {
+			delayCounter--;
+		}
+	}
+
+	public EnemyGenInfo getNextEnemyInfo() {
+		EnemyGenInfo genInfo = null;
+		if (enemyGenInfoQueue.peek() != null) {
+			genInfo = enemyGenInfoQueue.remove();
+		}
+
+		//For debugging
+		if (delayCounter > 0) {
+			System.out.println("Enemy generated before countdown complete");
+		}
+
+		delayCounter = GENERATION_DELAY;
+
+		return genInfo;
+	}
+}
